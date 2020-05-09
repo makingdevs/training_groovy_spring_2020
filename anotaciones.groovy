206d761be7fe9a25cff97174855d7cb90c52db06
technosoul <rockdrigo.mtz@gmail.com>
2020-05-09 09:59:04 -0500

    agregando ejemplo de anotaciones

---
+++ anotaciones.groovy
@@ -0,0 +1,9 @@
+import static org.junit.Assert.assertEquals
+import org.junit.Test
+
+class SimpleTest {
+  @Test
+  void shouldAdd() {
+    assertEquals("Groovy should add correctly", 2, 1 + 1)
+  }
+}
