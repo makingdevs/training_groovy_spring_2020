9938a89d4b9ed6daab9a7dcf436866119af68a2c
technosoul <rockdrigo.mtz@gmail.com>
2020-05-09 08:18:40 -0500

    agregando ejemplo de bigdecimal

--- primitivos.groovy
+++ primitivos.groovy
@@ -3,9 +3,11 @@ double c = 2.0
 float b = 3.0
 char d = 'a'
 boolean e = true
+z = 1.10 * 34.9864
 
 println a.class
 println c.class
 println b.class
 println d.class
 println e.class
+println z.class
