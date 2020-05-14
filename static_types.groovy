22bb80bdfd943fac7513837848e9f3b6c32089c9
neodevelop <juan@makingdevs.com>
2020-05-13 19:39:12 -0500

    The good types!!

--- static_types.groovy
+++ static_types.groovy
@@ -1,28 +1,34 @@
-def c(s){
+import groovy.transform.TypeChecked
+
+@TypeChecked
+def c(String s){
   println s.toUpperCase()
 }
 c("hola")
 c('c')
 // c(4)
 
+@TypeChecked
 class Person {
   String firstName
   String lastName
-  String getFullName() { "$firstName $lastname" }
+  String getFullName() { "$firstName $lastName" }
 }
 def p = new Person()
 
+@TypeChecked
 int metodo(){
-  if(false){ 'String' }
+  if(false){ 0 }
   else { 42 }
 }
 
 metodo()
 
+@TypeChecked
 def shout(String str) {
   println "Printing in uppercase"
   println str.toUpperCase()
   println "Printing again in uppercase"
-  //println str.toUppercase()
+  println str.toUpperCase()
 }
 shout('hello')
