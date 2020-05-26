cd19a14c831a3ef6d8b6359cefa5abe870262844
neodevelop <juan@makingdevs.com>
2020-05-26 16:49:24 -0500

    Create JSON

--- groovy_productivty/parse_json.groovy
+++ groovy_productivty/parse_json.groovy
@@ -1,10 +1,23 @@
 import groovy.json.*
 
-payload = new URL("https://api.github.com/users/neodevelop/repos").text
+//payload = new URL("https://api.github.com/users/neodevelop/repos").text
+//
+//jsonSlurper = new JsonSlurper()
+//document = jsonSlurper.parseText(payload)
+//
+//println document
+//println document.size()
+//println document*.name
 
-jsonSlurper = new JsonSlurper()
-document = jsonSlurper.parseText(payload)
+jsonBuilder = new JsonBuilder()
 
-println document
-println document.size()
-println document*.name
+jsonBuilder.training {
+  name "Groovy productivity"
+  duration 10
+  instructor "@neodevelop"
+  students { ["Alfonso", "Xiomara"] }
+}
+
+println jsonBuilder.toString()
+println JsonOutput.prettyPrint(jsonBuilder.toString())
+
