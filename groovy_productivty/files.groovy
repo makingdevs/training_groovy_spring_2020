213ff287ff22424622e23bf81f97b708e55f0c4a
neodevelop <juan@makingdevs.com>
2020-05-26 15:49:10 -0500

    Explore files

--- groovy_productivty/files.groovy
+++ groovy_productivty/files.groovy
@@ -9,3 +9,13 @@ println "-"*100
 root.eachFile {
   println it
 }
+println "#"*100
+
+f2 = new File("parenthesis.txt")
+println f2.name
+println f2.absolutePath
+println f2.directory
+
+println f2.text
+println f2.class
+
