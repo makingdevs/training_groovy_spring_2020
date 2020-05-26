616897d602a16a352d3b3e410075f2091dc1533e
neodevelop <juan@makingdevs.com>
2020-05-26 15:53:22 -0500

    Explore text files

--- groovy_productivty/files.groovy
+++ groovy_productivty/files.groovy
@@ -19,3 +19,10 @@ println f2.directory
 println f2.text
 println f2.class
 
+f3 = new File("the_early_history_of_smalltalk.txt")
+lines = f3.readLines()
+println lines.size()
+println lines.class
+println lines*.size()
+// lines.each { line -> println line }
+f3.splitEachLine("\n") { line ->  }
