5204e595b758487d86ea5aed9620f5ba67345084
neodevelop <juan@makingdevs.com>
2020-05-26 16:02:57 -0500

    Copy binary files

--- groovy_productivty/files2.groovy
+++ groovy_productivty/files2.groovy
@@ -5,3 +5,8 @@ file.append("Entrneamiento en Groovy para ser más productivo\n")
 file << "Esto va a ponerse al final del archivo\n"
 
 new File("hello_copy.txt").write(file.text)
+
+document = new File("git-cheat-sheet_jrebel.pdf")
+new File("copy.pdf").withOutputStream { out ->
+  out.write(document.bytes)
+}
