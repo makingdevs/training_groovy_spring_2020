13112f95f33b1270df02ffb05648a62123f4fd6f
neodevelop <juan@makingdevs.com>
2020-05-28 15:42:19 -0500

    Use grab in script

--- groovy_productivty/spring.groovy
+++ groovy_productivty/spring.groovy
@@ -1,12 +1,16 @@
-@GrabConfig(systemClassLoader=true)
 @Grapes([
-  @Grab(group='mysql', module='mysql-connector-java', version='6.0.6'),
+  @Grab(group='mysql', module='mysql-connector-java', version='5.1.49'),
   @Grab(group='org.springframework', module='spring-jdbc', version='5.2.6.RELEASE')
 ])
+@GrabConfig(systemClassLoader=true)
 
 import org.springframework.jdbc.core.JdbcTemplate
 import org.springframework.jdbc.datasource.DriverManagerDataSource
 
-dataSource = new DriverManagerDataSource("jdbc:mysql://localhost/grooyy_training", "makingdevs", "makingdevs")
+dataSource = new DriverManagerDataSource("jdbc:mysql://localhost/groovy_training", "makingdevs", "makingdevs")
+dataSource.driverClassName = "com.mysql.jdbc.Driver"
 jdbcTemplate = new JdbcTemplate(dataSource: dataSource)
 println jdbcTemplate.properties
+
+counter = jdbcTemplate.queryForObject("select count(*) from issue", Integer)
+println "There are ${counter} issues"
