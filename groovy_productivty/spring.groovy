96af7b7c5e845a09259668324aa7b3f9ad418613
neodevelop <juan@makingdevs.com>
2020-05-28 15:27:24 -0500

    Update DB conecction

--- groovy_productivty/spring.groovy
+++ groovy_productivty/spring.groovy
@@ -1,9 +1,12 @@
+@GrabConfig(systemClassLoader=true)
 @Grapes([
   @Grab(group='mysql', module='mysql-connector-java', version='6.0.6'),
   @Grab(group='org.springframework', module='spring-jdbc', version='5.2.6.RELEASE')
 ])
 
 import org.springframework.jdbc.core.JdbcTemplate
-import org.springframework.jdbc.core.datasource.DriverManagerDataSource
+import org.springframework.jdbc.datasource.DriverManagerDataSource
 
-datasource = new DriverManagerDataSource("com.mysql.jdbc.Driver", "jdbc:mysql://localhost/grooyy_training", "makingdevs", "makingdevs")
+dataSource = new DriverManagerDataSource("jdbc:mysql://localhost/grooyy_training", "makingdevs", "makingdevs")
+jdbcTemplate = new JdbcTemplate(dataSource: dataSource)
+println jdbcTemplate.properties
