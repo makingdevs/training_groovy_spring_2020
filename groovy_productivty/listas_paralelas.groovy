fb3521fbe8ce449b973eb1521d6c9836515f12d3
neodevelop <juan@makingdevs.com>
2020-05-30 09:39:22 -0500

    Add benchmark function

--- groovy_productivty/listas_paralelas.groovy
+++ groovy_productivty/listas_paralelas.groovy
@@ -1,7 +1,15 @@
+def benchmark(function) {
+  def startTime = System.currentTimeMillis()
+  function()
+  def endTime = System.currentTimeMillis()
+  println "Total time: ${endTime-startTime}"
+}
 
 procesos = [300,500,100,900,400,300,400,1100,1500,100,200]
 
-procesos.each { t ->
-  Thread.sleep(t)
-  println "Finish job: ${t}"
+benchmark {
+  procesos.each { t ->
+    Thread.sleep(t)
+    println "Finish job: ${t}"
+  }
 }
