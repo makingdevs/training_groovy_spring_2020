93ed027543f83a9c1bea5e522cbccd642957645d
neodevelop <juan@makingdevs.com>
2020-06-01 16:41:45 -0500

    Change WSDL service

--- groovy_productivty/soap_operations.groovy
+++ groovy_productivty/soap_operations.groovy
@@ -1,6 +1,6 @@
 import groovy.xml.Namespace
 
-url = "http://wsf.cdyne.com//WeatherWS/Weather.asmx?WSDL"
+url = "http://currencyconverter.kowabunga.net/converter.asmx?WSDL"
 wsdl = new Namespace("http://schemas.xmlsoap.org/wsdl/","wsdl")
 doc = new XmlParser().parse(url)
 println doc[wsdl.portType][wsdl.operation].'@name'
