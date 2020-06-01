caa3a52acf18228b4e4eb7ff34cb135099ca6a81
neodevelop <juan@makingdevs.com>
2020-06-01 17:06:43 -0500

    Use GPars w/webservices

--- groovy_productivty/soap_client.groovy
+++ groovy_productivty/soap_client.groovy
@@ -1,20 +1,30 @@
 @Grab('com.github.groovy-wslite:groovy-wslite:1.1.2')
 import wslite.soap.*
+import static groovyx.gpars.GParsPool.withPool
 
-url = "http://currencyconverter.kowabunga.net/converter.asmx?WSDL"
-client = new SOAPClient(url)
-action = "http://tempuri.org/GetConversionAmount"
+def requestSoap(Date date) {
+  url = "http://currencyconverter.kowabunga.net/converter.asmx?WSDL"
+  client = new SOAPClient(url)
+  action = "http://tempuri.org/GetConversionAmount"
 
-response = client.send(SOAPAction: action) {
-  body {
-    GetConversionAmount(xmlns: "http://tempuri.org/") {
-      CurrencyFrom "USD"
-      CurrencyTo "MXN"
-      RateDate "2020-01-26T19:32:52Z"
-      Amount 1
+  response = client.send(SOAPAction: action) {
+    body {
+      GetConversionAmount(xmlns: "http://tempuri.org/") {
+        CurrencyFrom "USD"
+        CurrencyTo "MXN"
+        RateDate date.format("yyyy-MM-dd'T'HH:mm:ss'Z'", TimeZone.getTimeZone("UTC"))
+        Amount 1
+      }
     }
   }
+  response.GetConversionAmountResponse
 }
 
-println response.httpResponse.statusCode
-println response.GetConversionAmountResponse
+today = new Date()
+
+withPool {
+  (today-40..today-20).collectParallel { d ->
+  //rates = (today-60..today-10).collect { d ->
+    println "${d.format('dd-MM-yyyy')} : " + requestSoap(d)
+  }
+}
