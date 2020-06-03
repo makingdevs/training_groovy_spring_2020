60a2990d29065369621d788b94fd930ddcc7c972
neodevelop <juan@makingdevs.com>
2020-06-03 15:46:19 -0500

    Add session, use specific methods

--- groovy_productivty/hello_web.groovy
+++ groovy_productivty/hello_web.groovy
@@ -1,9 +1,18 @@
+if(!session) session = request.getSession(true)
+if(!session.username) session.username = "MakingDevs"
+
 html.html {
   head {
     title "Hello world in Groovlets"
   }
   body {
     h1 "Welcome to Groovlets in MakingDevs"
+    ul {
+      li "Method ${request.method}"
+      li "URI ${request.requestURI}"
+      li "Application ${context}"
+      li "Params ${params}"
+    }
     table(summaery: 'binding') {
       tbody {
         binding.variables.each { key, value ->
