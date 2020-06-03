b3ff46a37f2e855d244e4d01826193c2a0da7f0c
neodevelop <juan@makingdevs.com>
2020-06-03 15:16:44 -0500

    Use SimpleTemplateEngine

---
+++ groovy_productivty/ste.groovy
@@ -0,0 +1,18 @@
+def binding = [
+  firstname : "Grace",
+  lastname  : "Hopper",
+  accepted  : true,
+  title     : 'Groovy for COBOL programmers'
+]
+def engine = new groovy.text.SimpleTemplateEngine()
+def text = '''\
+  Dear <%= firstname %> $lastname,
+
+  We <% if (accepted) print 'are pleased' else print 'regret' %> \
+  to inform you that your paper entitled
+  '$title' was ${ accepted ? 'accepted' : 'rejected' }.
+
+  The conference committee.
+'''
+def template = engine.createTemplate(text).make(binding)
+println template.toString()
