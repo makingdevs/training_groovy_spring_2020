75b4a6ddfeb33240fcbdc24b3e99df6484dcef73
neodevelop <juan@makingdevs.com>
2020-06-03 15:36:35 -0500

    Explore variables in Groovlets

--- groovy_productivty/hello_web.groovy
+++ groovy_productivty/hello_web.groovy
@@ -4,5 +4,15 @@ html.html {
   }
   body {
     h1 "Welcome to Groovlets in MakingDevs"
+    table(summaery: 'binding') {
+      tbody {
+        binding.variables.each { key, value ->
+          tr {
+            td key.toString()
+            td(value ? value.toString() : 'n/a')
+          }
+        }
+      }
+    }
   }
 }
