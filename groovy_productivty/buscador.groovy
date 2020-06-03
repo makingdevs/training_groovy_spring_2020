8444965d57575c3a96dc062b79f496fbc46f9c50
neodevelop <juan@makingdevs.com>
2020-06-03 16:09:05 -0500

    Search for breweries and display in template

--- groovy_productivty/buscador.groovy
+++ groovy_productivty/buscador.groovy
@@ -2,4 +2,8 @@ engine = new groovy.text.SimpleTemplateEngine()
 source = new File("buscador.template.html").text
 template = engine.createTemplate(source)
 
-out << template.make([:])
+breweries = []
+if(params.q)
+  breweries = BuscadorDeCervecerias.instance.findByPais(params.q)
+
+out << template.make([message:"Cerveceras encontradas...", breweries: breweries])
